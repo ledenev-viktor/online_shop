e96744a2ac26c1f784f8a5a689071a2b91817946
ledenev-viktor <ledenev4252@gmail.com>
2024-01-06 18:34:26 +0300

    прокинул модули в app модуль, добавил swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeConfigService } from './config/sequelizeConfig.service';
+import { databaseConfig } from './config/configuration';
+import { AuthModule } from './auth/auth.module';
+import { BoilerPartsModule } from './boiler-parts/boiler-parts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    UsersModule,
+    AuthModule,
+    BoilerPartsModule,
+  ],
 })
 export class AppModule {}
